4d4fd778e3b1a5db0d3bfcfecb4cc0157092365e
water <130329555+water783@users.noreply.github.com>
2025-01-08 14:38:30 +0800

    add kind 20

--- groups.0xchat.com/main.go
+++ groups.0xchat.com/main.go
@@ -74,7 +74,7 @@ func main() {
 		policies.PreventLargeTags(640),
 		policies.PreventTooManyIndexableTags(6, []int{9000, 9001, 9003, 9004, 9005}, nil),
 		policies.RestrictToSpecifiedKinds(
-			7, 9, 10, 11, 12, 16, 1018, 1068, 1111,
+			7, 9, 10, 11, 12, 16, 20, 1018, 1068, 1111,
 			30023, 31922, 31923, 9802,
 			9000, 9001, 9002, 9003, 9004, 9005, 9006, 9007, 9008,
 			9021, 9022, 9735, 34235, 34236,
